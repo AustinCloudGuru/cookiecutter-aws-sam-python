245b7516610626a9b38bb7c23fcd9acef8652c9d
Mark Honomichl <mark@honomichl.net>
2020-12-30 14:38:51 -0600

    Adding logging

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.function_name }}/handler.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.function_name }}/handler.py
@@ -1,5 +1,12 @@
+import logging
 import boto3
-import json
+from botocore.exceptions import ClientError
+
+# Configure the Logger
+logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s %(message)s")
+logger = logging.getLogger(__name__)
+logger.setLevel("INFO")
+
 
 def lambda_handler(event, context):
     return "It works!"
